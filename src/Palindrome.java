95fa56cbc88478e5471a86903ac83527ea8c1590
Maciej Lipski <maciej.lipski2.stud@pw.edu.pl>
2022-11-07 17:07:02 +0100

    Rozwiązanie działania 1.0

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -11,9 +11,31 @@ public class Palindrome {
     }
     return true;
     }
-
+    public static String analyzeSubstring (String word){
+        if(isPalindrome(word)){
+            return word;
+        }
+        else return "";
+}
+    public static String findLongestPalindrome(String word){
+        String longestPalindrome="";
+        String current="";
+        for (int i=0;i<word.length();i++){
+            String subword=word.substring(i,word.length());
+            int index=subword.length();
+            while (index>0){
+                String substring=subword.substring(0,index);
+                    current=analyzeSubstring(substring);
+                if(longestPalindrome.length()<current.length())
+                    longestPalindrome=current;
+                index--;
+            }
+        }
+        return longestPalindrome;
+    }
     public static void main(String[] args) {
-        System.out.println(isPalindrome("abba"));
+        String longest=findLongestPalindrome("mama");
+        System.out.println(longest);
     }
 }
 
